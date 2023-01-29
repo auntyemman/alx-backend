8d52ef46fbeedfbb7997c2637504a71617e1a56c
auntyemman <1mole.coco@gmail.com>
2023-01-28 23:03:25 -0800

    Initial commit

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,25 +1,13 @@
 #!/usr/bin/env python3
-
+'''
+    Simple helper function.
 '''
 
-    Simple helper function.
 
+def index_range(page, page_size):
     '''
-
-
-
-
-
-    def index_range(page, page_size):
-
-            '''
-
-                    Returns the range of indexes for a given page.
-
-                        '''
-
-                            start = (page - 1) * page_size
-
-                                end = page * page_size
-
-                                    return start, end
+        Returns the range of indexes for a given page.
+    '''
+    start = (page - 1) * page_size
+    end = page * page_size
+    return start, end
